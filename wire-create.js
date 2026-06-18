18621260b4f6e01a604d1105b55086d47f708254
John Jacobs <55412930+john-jacobs@users.noreply.github.com>
2026-06-18 13:47:02 -0700

    Bust cache for Wire create auth handling

--- wire-create.js
+++ wire-create.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("#wireCreateForm");
-const APP_VERSION = "20260618-wirecreate-debug1";
+const APP_VERSION = "20260618-wirecreate-authfix1";
 const notes = document.querySelector("#wireNotes");
 const locationInput = document.querySelector("#wireLocation");
 const resultInput = document.querySelector("#wireResult");
